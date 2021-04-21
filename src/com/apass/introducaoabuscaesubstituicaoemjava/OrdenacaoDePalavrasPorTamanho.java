43fb8374b7fadd708476e0cdcd7720dfdb44c406
Anderson Passos Silva <anders.ps02@gmail.com>
2021-04-21 15:45:53 -0300

    Finaliza atividade 1

--- src/com/apass/introducaoabuscaesubstituicaoemjava/OrdenacaoDePalavrasPorTamanho.java
+++ src/com/apass/introducaoabuscaesubstituicaoemjava/OrdenacaoDePalavrasPorTamanho.java
@@ -1,30 +1,37 @@
-package com.apass.introducaoabuscaesubstituicaoemjava;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Stream;
-
 
 // 1. Ordenação de palavras por tamanho
 
 public class OrdenacaoDePalavrasPorTamanho {
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		var numeroTestes = Integer.parseInt(br.readLine());
-		
-		List<List<String>> testes = new ArrayList<List<String>>();
-		
-		for(var contador = 0; contador < numeroTestes; contador ++) {
-			testes.add(List.of(br.readLine().split(" "));
-		}
-		
-		for(var teste: testes) {
-			System.out.println();
-		}
+    public static void main(String[] args) throws NumberFormatException, IOException {
+
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+	var numeroTestes = Integer.parseInt(br.readLine());
+
+	List<List<String>> testes = new ArrayList<List<String>>();
+
+	for (var contador = 0; contador < numeroTestes; contador++) {
+	    testes.add(List.of(br.readLine().split(" ")));
 	}
+
+	testes.forEach(teste -> {
+	    teste.stream().sorted((a, b) -> {
+		var coparacaoTamanho = Integer.compare(a.length(), b.length()) * -1;
+		if (coparacaoTamanho == 0) {
+		    return a.compareToIgnoreCase(b);
+		}
+		return coparacaoTamanho;
+	    }).forEach(palavra -> System.out.print(palavra + " "));
+	    System.out.println();
+	});
+
+    }
 }
